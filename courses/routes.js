b04e7ea4cd5ced0bcae7b00c51839d04c1ee8b39
na-rachl <ma.ra@northeastern.edu>
2023-11-20 22:39:34 -0500

    initial

--- courses/routes.js
+++ courses/routes.js
@@ -1,13 +1,14 @@
 import Database from "../Database/index.js";
+
 function CourseRoutes(app) {
     console.log("hi");
 
     //retrieve a course by thier id
     app.get("/api/courses/:id", (req, res) => {
 
-        const { id } = req.params;
-        console.log("ID:",id);
-        console.log("courses:",Database.courses);
+        const {id} = req.params;
+        console.log("ID:", id);
+        console.log("courses:", Database.courses);
 
         const course = Database.courses.find((c) => c._id === id);
         if (!course) {
@@ -20,12 +21,11 @@ function CourseRoutes(app) {
     });
 
 
-
     app.put("/api/courses/:id", (req, res) => {
-        const { id } = req.params;
+        const {id} = req.params;
         const course = req.body;
         Database.courses = Database.courses.map((c) =>
-            c._id === id ? { ...c, ...course } : c
+            c._id === id ? {...c, ...course} : c
         );
         console.log("backend add got called");
         res.sendStatus(204);
@@ -33,7 +33,7 @@ function CourseRoutes(app) {
 
 
     app.delete("/api/courses/:id", (req, res) => {
-        const { id } = req.params;
+        const {id} = req.params;
         console.log("backend delete got called");
         Database.courses = Database.courses.filter((c) => c._id !== id);
         res.sendStatus(204);
@@ -41,8 +41,10 @@ function CourseRoutes(app) {
 
 
     app.post("/api/courses", (req, res) => {
-        const course = { ...req.body,
-            _id: new Date().getTime().toString() };
+        const course = {
+            ...req.body,
+            _id: new Date().getTime().toString()
+        };
         Database.courses.push(course);
         res.send(course);
     });
@@ -53,4 +55,5 @@ function CourseRoutes(app) {
     //     res.send(courses);
     // });
 }
+
 export default CourseRoutes;
